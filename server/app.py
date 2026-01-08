4eea67ae4f700ffb3be6eab9cb62c4ee8e389979
allanndunda <allanndunda>
2026-01-08 09:13:12 +0300

    done

--- server/app.py
+++ server/app.py
@@ -14,13 +14,46 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
-
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
     return ''
 
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(
+        body=data['body'],
+        username=data['username']
+    )
+    db.session.add(new_message)
+    db.session.commit()
+    response = make_response(new_message.to_dict(), 201)
+    return response
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get(id)
+
+    if not message:
+        return jsonify({"error": "Message not found"}), 404
+
+    if request.form.get('body'):
+        message.body = request.form.get('body')
+
+    db.session.commit()
+
+    return jsonify(message.to_dict()), 200
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+
+    if not message:
+        return jsonify({"error": "Message not found"}), 404
+
+    db.session.delete(message)
+    db.session.commit()
+
+    return '', 204
 if __name__ == '__main__':
     app.run(port=5555)
